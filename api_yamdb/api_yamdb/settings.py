88598e47a98280aa426e71850be6599dc93a47e1
Anton Kuznetsov <kuzantiv1992@mail.ru>
2023-04-25 12:34:01 +0600

    Для корректной работы ресурса redoc/ изменены настройки.

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -39,7 +39,7 @@ TEMPLATES_DIR = BASE_DIR / 'templates'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
