e4ceed0a24ca014228aa1d04fc56d950eb32aef6
Николай Хван <khvannickolay@gmail.com>
2023-04-28 11:03:31 +0500

    Добавлены валидаторы и рейтинг

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,14 +1,26 @@
+from django.core.validators import RegexValidator
 from django.db import models
+from django.db.models import Avg
+
+from .validators import validate_rating
 
 
 class Category(models.Model):
     name = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=50, unique=True)
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        validators=[RegexValidator(r'^[-a-zA-Z0-9_]+$')]
+    )
 
 
 class Genre(models.Model):
     name = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=50, unique=True)
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        validators=[RegexValidator(r'^[-a-zA-Z0-9_]+$')]
+    )
 
 
 class Title(models.Model):
@@ -17,19 +29,30 @@ class Title(models.Model):
     description = models.TextField(null=True, blank=True)
     genre = models.ManyToManyField(Genre)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    rating = ...
+    
+    @property
+    def rating(self):
+        return self.reviews.aggregate(Avg('score'))
 
 
 class Review(models.Model):
     #author = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.ForeignKey(Title, on_delete=models.CASCADE)
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
     text = models.TextField()
-    score = models.IntegerField()
+    score = models.IntegerField(validators=[validate_rating])
     pub_date = models.DateTimeField(auto_now_add=True)
 
 
 class Comment(models.Model):
     #author = models.ForeignKey(User, on_delete=models.CASCADE)
-    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
