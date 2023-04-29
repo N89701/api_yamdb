ffc5388edde5d57b69d89bee2cc460c0a286c685
Anton Kuznetsov <kuzantiv1992@mail.ru>
2023-04-29 23:53:13 +0600

    исправление, Вместо DEFAULT_PERMISSON_CLASSES УДАЛИЛ DEFAULT_AUTHENTICATION_CLASSES,

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -107,8 +107,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
 
