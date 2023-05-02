9999705cc27fb4443de73306915700f0d66e65c0
Николай Хван <khvannickolay@gmail.com>
2023-05-02 12:00:11 +0500

    merge dev+dev_reviews

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -3,5 +3,5 @@ from django.core.exceptions import ValidationError
 
 def validate_rating(value):
     if value < 1 or value > 10:
-        raise ValidationError('Your score must be between 1 and 10 inclsive')
+        raise ValidationError('Your score must be between 1 and 10 inclusive')
     return True
